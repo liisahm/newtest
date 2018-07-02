afe540028778c8e0989e7de45c57bc4b4cee6d77
Liisa Haabpiht <liisa.haabpiht@gmail.com>
2018-07-02 12:03:39 +0300

    changed https to http

--- Zite/Startup.cs
+++ Zite/Startup.cs
@@ -28,11 +28,11 @@ namespace Zite
       services.AddCors(options =>
       {
         options.AddPolicy("AllowSpecificOrigin",
-            builder => builder.WithOrigins("https://localhost:49866"));
+            builder => builder.WithOrigins("http://localhost:49866"));
         options.AddPolicy("AllowAllHeaders",
                     builder =>
                     {
-                      builder.WithOrigins("https://localhost:49866")
+                      builder.WithOrigins("http://localhost:49866")
                              .AllowAnyHeader();
                     });
       });
@@ -48,7 +48,7 @@ namespace Zite
         }
 
       app.UseCors((builder =>
-        builder.WithOrigins("https://localhost:49866")
+        builder.WithOrigins("http://localhost:49866")
             .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod()));
